74271fa3b9ba45e0e7aaca5a8f223bdb9d86ba63
ZStout10 <68234015+ZStout10@users.noreply.github.com>
2021-01-23 23:51:58 -0500

    Add files via upload

---
+++ Module 01/M01 Chapter 1 Tutorial/plants.js
@@ -0,0 +1,15 @@
+/* JavaScript 6th Edition
+	Chapter 1
+	Chapter Case
+	
+	Tinley Xeriscapes
+	Author: Zach Stout
+	Date: 1/23/2021
+	
+	Filename: plants.js
+*/
+
+//define variables containing img src values
+var blanket = "images/blanket.jpg";
+var bluestem = "images/bluestem.jpg";
+var rugosa = "images/rugosa.jpg";
